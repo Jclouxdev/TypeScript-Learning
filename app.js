4761e1ca64ab995c82e907bf4ccb60fb5d964d51
Jdev <joseph.clx@gmail.com>
2022-01-30 18:23:31 +0100

    simple-array-types

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
-    name: 'André',
-    age: 25
+    name: "André",
+    age: 25,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -5,6 +5,14 @@
 const person = {
   name: "André",
   age: 25,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}

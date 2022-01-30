93931755ec531e53dfc7c6f085a892984f0875be
Jdev <joseph.clx@gmail.com>
2022-01-30 18:36:57 +0100

    simple-tuple-types

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 var person = {
     name: "André",
     age: 25,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+// person.role.push('admin');
+// person.role[1] = 10;
+person.role = [0, 'admin'];
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
 }
--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]
+} = {
   name: "André",
   age: 25,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+person.role = [0, 'admin'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];

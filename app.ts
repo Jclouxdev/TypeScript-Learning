553657340bf312cb1c39db2c8abb4aba96eaacd8
Jdev <joseph.clx@gmail.com>
2022-01-31 16:11:20 +0100

    simple-enums-type

--- app.ts
+++ app.ts
@@ -2,22 +2,35 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: "André",
+//   age: 25,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
   name: "André",
   age: 25,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 // person.role.push('admin');
 // person.role[1] = 10;
 
-person.role = [0, 'admin'];
+//person.role = [0, 'admin'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -26,4 +39,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin');
 }
